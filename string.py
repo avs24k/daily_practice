58865fe9e1734230c9f4eea79ee980d5b6ce5a45
avs_24k <patilavinash502@gmail.com>
2023-09-09 21:01:10 +0530

    09 september 2023

---
+++ string.py
@@ -0,0 +1,53 @@
+"""1. Write a Python program to calculate the length of a string.
+"""
+# A = "Avinash"
+# B = 0
+# for i in A:
+#     B +=1
+# print(B)
+
+"""2. Write a Python program to count the number of characters (character frequency) in a string.
+Sample String : google.com'
+Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}"""
+# A = "google.com"
+# B = {}
+# for i in A:
+#     if i in B:
+#         B[i] += 1
+#     else:
+#         B[i] = 1
+# print(B)
+
+"""3. Write a Python program to get a string made of the first 2 and last 2 characters of a given string. 
+If the string length is less than 2, return the empty string instead.
+Sample String : 'w3resource'
+Expected Result : 'w3ce'
+Sample String : 'w3'
+Expected Result : 'w3w3'
+Sample String : ' w'
+Expected Result : Empty String"""
+
+# string = 'w3resource'
+#
+# if len(string)> 2-1:
+#     print(string[0:2],string[-2:])
+# else:
+#     print("empty string")
+
+"""4. Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself.
+Sample String : 'restart'
+Expected Result : 'resta$t'"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
