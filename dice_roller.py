30e3f6610cb8d79b1d78ce5b8f276aeca07e46fe
avs_24k <patilavinash502@gmail.com>
2023-09-19 00:18:56 +0530

    date - 19/09/2023, time- 0.18

---
+++ dice_roller.py
@@ -0,0 +1,15 @@
+# import random
+#
+#
+# def roll_dice():
+#     return random.randint(1, 6)
+#
+#
+# user_input = input("Want to roll the dice Yes/No", )
+#
+# while user_input.lower() == "yes":
+#     result = roll_dice()
+#     print(f"you rolled:{result}")
+#     user_input = input("roll again?")
+# print("thank you for playing")
+
